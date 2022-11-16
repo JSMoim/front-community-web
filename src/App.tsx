df9ead1eb78242a3f141ffbce2c5b17ff2c27329
Hyeongyu-619 <rlagusrb619@gmail.com>
2022-11-16 17:18:29 +0900

    tab test

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,62 @@ import { useState } from "react";
 import reactLogo from "@/assets/react.svg";
 import githubLogo from "@/assets/github.svg";
 import "@/App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom";
 import MainPage from "@/pages/MainPage";
-import AboutPage from "@/pages/AboutPage";
+import NoticePage from "@/pages/NoticePage";
 import QnaPage from "@/pages/QnaPage";
 import CommunityPage from "@/pages/CommunityPage";
+import { ThemeProvider } from "@emotion/react";
+import { Box, Tabs, Tab, Grid, Divider, Stack, Button } from "@mui/material";
+import React from "react";
+import { DefaultTheme } from "./themes/DefaultThemes";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
 
   return (
-    <div className="App">
-      <div className="topnav">
-        <a className="active" href="/">
-          <img src="src/assets/unnamed.png" width={"40px"} height={"40px"} />
-        </a>
-        <a href="/qna">Q & A</a>
-        <a href="/community">커뮤니티</a>
-        <a href="/about">About</a>
-      </div>
+    <div>
+      <Box sx={{ Width: 100, height: 100, bgcolor: "background.paper" }}>
+        <Stack
+          direction="row"
+          justifyContent={"flex-start"}
+          alignItems={"flex-start"}
+          spacing={4}
+        >
+          <div className="MainIcon">
+            <a className="active" href="/">
+              <img
+                src="src/assets/unnamed.png"
+                width={"40px"}
+                height={"40px"}
+              />
+            </a>
+          </div>
+          <ThemeProvider theme={DefaultTheme}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              variant="scrollable"
+              scrollButtons="auto"
+              aria-label="scrollable auto tabs example"
+            >
+              <Tab label="Q&A" to="/qna" component={Link} />
+              <Tab label="커뮤니티" to="/community" component={Link} />
+              <Tab label="공지사항" to="/notice" component={Link} />
+            </Tabs>
+          </ThemeProvider>
+        </Stack>
+      </Box>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/qna" element={<QnaPage />} />
           <Route path="/community" element={<CommunityPage />} />
-          <Route path="/about" element={<AboutPage />} />
+          <Route path="/notice" element={<NoticePage />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/themes/DefaultThemes.tsx
@@ -0,0 +1,29 @@
+import { createTheme } from "@mui/material/styles";
+import { purple, grey, blueGrey } from "@mui/material/colors";
+
+export const DefaultTheme = createTheme({
+  palette: {
+    primary: {
+      main: blueGrey[800],
+    },
+    secondary: {
+      main: "#ffffff",
+    },
+  },
+  components: {
+    MuiTab: {
+      defaultProps: {
+        sx: {
+          width: 60,
+        },
+      },
+    },
+    MuiTabs: {
+      defaultProps: {
+        sx: {
+          width: "100%",
+        },
+      },
+    },
+  },
+});
